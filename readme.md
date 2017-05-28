9ab28493c361017506f2d144e80cb44a88785a5f
jay <vanjaydo@gmail.com>
2017-05-28 23:26:49 +0800

    Version 1.1 Add the auto-reply future (used to reply the comments which @ you automatically )

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 This script is used to click the "like" button automatically in Qzone.
 It is created by Liu Yang (my roommate,his github addr is https://github.com/lylllcc) and me.
 If it could help you, our porpose is reached.
+
+To use it, you have to set you account & password & nickname according to reminders.
+All you have to set include ACCOUNT/PASSWORD/myAccount/myNickName.
--- zanScript.py
+++ zanScript.py
@@ -1,58 +1,98 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 ACCOUNT="******"#改为账号
 PASSWORD="*****"#改为密码
 
 ZAN="""
+	//点赞
+	var myAccount="******";//设置自己的账号
+
 	var all=document.getElementsByClassName("item qz_like_btn_v3");
-	console.log("Finding unclicked");
 	for(var i=0;i<all.length;i++)
 	{ 
 		if(all[i].getAttribute("data-clicklog")=="like")
 		{
 			target=all[i].getAttribute("data-unikey");
-			all[i].click();
-			console.log("Just clicked the-> "+target+" <-");//其中target为说说地址，可在浏览器粘贴直接访问
+			if(target.indexOf(myAccount)==-1)//不点自己的说说，需要的话可以去掉该判断条件
+			{
+				all[i].click();
+				if(all[i].getAttribute("data-clicklog")=="like") all[i].click();
+				console.log("Just clicked the-> "+target+" <-");//其中target为说说地址，可在浏览器粘贴直接访问
+			}
 		}
-	}"""
+	}
+	"""
+
+REPLY="""
+	//下面对@本人的评论进行自动回复
+	var myNickName="*****";//设置自己的qq昵称
+	
+	var autoReply="[自动回复]本人空间已交由爬虫进行自动点赞和回复，有问题请直接联系我，谢谢。";
+	var comment=document.getElementsByClassName("comments-item bor3");
+	for(var j=0;j<comment.length;j++)
+	{
+	    var content=comment[j].getElementsByClassName("comments-content");
+	    var allAt=content[0].getElementsByClassName("nickname name c_tx  q_namecard");
+	    for(var i=0;i<allAt.length;i++)
+	    {
+	        if((allAt[i].innerText).indexOf("@"+myNickName)!=-1)
+	        {
+	            if(comment[j].getElementsByClassName("comments-list mod-comments-sub").length==0)
+	            {
+	                content[0].getElementsByClassName("act-reply none")[0].click();
+	                var box=document.getElementsByClassName("comment-box-wrap")[0];
+	                box=document.getElementsByClassName("comment-box-wrap")[0];
+                	if(box==null)
+	                	console.log("click is null");
+	       			else
+	       			{
+                		box.getElementsByClassName("textinput textarea c_tx2")[0].innerText=autoReply;
+                		comment[j].getElementsByClassName("btn-post gb_bt  evt_click")[0].click();
+                		console.log("Just clicked auto-reply");
+	       			}
+	            }
+	        }
+	    }
+	}
+	"""
 
 def login(driver):
-	print ("Logging......")
-	driver.get("http://qzone.qq.com/")
-	time.sleep(3)
-	driver.switch_to_frame("login_frame")
-	time.sleep(3)
-	driver.find_element_by_id("switcher_plogin").click()
-	time.sleep(3)
-	driver.find_element_by_id("u").clear()  
-	driver.find_element_by_id("u").send_keys(ACCOUNT)
-	driver.find_element_by_id("p").clear()
-	driver.find_element_by_id("p").send_keys(PASSWORD)
-	time.sleep(3)
-	driver.find_element_by_id("login_button").click()
-	time.sleep(3)
-	return driver
+	try:
+		print ("Logging......")
+		driver.get("http://qzone.qq.com/")
+		time.sleep(3)
+		driver.switch_to_frame("login_frame")
+		time.sleep(3)
+		driver.find_element_by_id("switcher_plogin").click()
+		time.sleep(3)
+		driver.find_element_by_id("u").clear()  
+		driver.find_element_by_id("u").send_keys(ACCOUNT)
+		driver.find_element_by_id("p").clear()
+		driver.find_element_by_id("p").send_keys(PASSWORD)
+		time.sleep(3)
+		driver.find_element_by_id("login_button").click()
+		time.sleep(3)
+	except Exception as e:
+		print("Login failed,\nplease check your account & password & network.\n")
+		driver.close()
+		exit(0)
+	else:
+		print("Login successfully.")
+		return driver
 
 if __name__=="__main__":
-	driver = webdriver.PhantomJS()#window下需填入executable_path="浏览器绝对路径"属性
+	driver = webdriver.PhantomJS()#window下需填入executable_path="浏览器绝对路径"属性	
 	while 1:
-		try:
-			driver.find_element_by_xpath('//a[@id="feed_tab_hover"]')
-		except NoSuchElementException:
+		a=driver.execute_script("return document.getElementById('feed_tab_hover')")
+		if a is None:
 			driver=login(driver)
-			try:
-				driver.find_element_by_xpath('//a[@id="feed_tab_hover"]')
-			except NoSuchElementException:
-				print("Login failed,\nplease check your account & password & network.\n")
-				exit(0)
-			else:
-				print("Login successfully.")
 		driver.execute_script(ZAN)
+		driver.execute_script(REPLY)
 		log=driver.get_log("browser")
 		for i in log:
-			if (i["message"].find("click")!=-1):
+			if (i["message"].find("clicked")!=-1):
 				print(str(i["message"]).encode("utf8"))
 		driver.refresh()
 		time.sleep(30)
