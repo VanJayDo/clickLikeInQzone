8f2ab3f18fa1eb100b5bab3548b1f6215adcdc64
jay <vanjaydo@gmail.com>
2017-06-07 09:02:04 +0800

    Version 1.2.1 stable

--- readme.md
+++ readme.md
@@ -1,7 +1,26 @@
-# clickLikeInQzone
-This script is used to click the "like" button automatically in Qzone.
-It is created by Liu Yang (my roommate,his github addr is https://github.com/lylllcc) and me.
-If it could help you, our porpose is reached.
+##   clickLikeInQzone
+对于空间里朋友们的动态，我们有时候觉得点赞不够及时或者会漏赞，基于这种需求，便有了这个脚本。<br>
+脚本依托于 [selenium](https://github.com/SeleniumHQ/selenium)模块，调用[phantomjs](https://github.com/ariya/phantomjs)实现功能。python2和3均可运行。<br>
+你需要手动改写脚本中ACCOUNT，PASSWORD，myAccount，myNickName等变量。
 
-To use it, you have to set you account & password & nickname according to reminders.
-All you have to set include ACCOUNT/PASSWORD/myAccount/myNickName.
+在运行时打印的日志中，
+
+```markdown
+Just clicked the-> URL <-
+
+```
+
+中间的url是刚刚所点的说说地址，如果你在浏览器中已登录，则粘贴该url可以直接访问该说说。<br>
+因为js的特性，一个说说有时可能需要经过多次才能点上赞（只点了一次），但是日志会打印多次，无妨。
+
+####**更新日志：**
+
+```markdown
+Version 1.2.1 修复了部分bug（程序跑飞，phantomjs会占用服务器过多资源等），在我目前所有的测试中都是通过的，较稳定。
+
+Version 1.1 新加了对于在评论里@本人的说说进行自动回复的功能。
+
+Version 1.0 测试版，实现点赞功能。
+``` 
+**注：**
+*该脚本运行在linux上时必须打印代码中的 js console日志，否则点赞无效（在我所有的测试中均如此）；windows上运行时则无所谓打印与否。*
--- zanScript.py
+++ zanScript.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+#!/bin/env python
+#coding: utf-8
+__author__="Vanjay Do"
+
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 import time
 
@@ -8,7 +12,6 @@ PASSWORD="*****"#改为密码
 ZAN="""
 	//点赞
 	var myAccount="******";//设置自己的账号
-
 	var all=document.getElementsByClassName("item qz_like_btn_v3");
 	for(var i=0;i<all.length;i++)
 	{ 
@@ -20,79 +23,97 @@ ZAN="""
 				all[i].click();
 				if(all[i].getAttribute("data-clicklog")=="like") all[i].click();
 				console.log("Just clicked the-> "+target+" <-");//其中target为说说地址，可在浏览器粘贴直接访问
+				break;//考虑到js异步的特点，每次只点一条
 			}
 		}
 	}
 	"""
-
 REPLY="""
-	//下面对@本人的评论进行自动回复
-	var myNickName="*****";//设置自己的qq昵称
-	
-	var autoReply="[自动回复]本人空间已交由爬虫进行自动点赞和回复，有问题请直接联系我，谢谢。";
-	var comment=document.getElementsByClassName("comments-item bor3");
-	for(var j=0;j<comment.length;j++)
+	//函数：对@本人的说说进行查找
+	function search(){
+		var myNickName="****";//设置直接的qq昵称
+		var myAccount="*****";//设置自己的账号
+		var comment=document.getElementsByClassName("comments-item bor3");
+		for(var j=0;j<comment.length;j++)
+		{
+		    var content=comment[j].getElementsByClassName("comments-content");
+		    var allAt=content[0].getElementsByClassName("nickname name c_tx  q_namecard");
+		    for(var i=0;i<allAt.length;i++)
+		    {
+		        if((allAt[i].innerText).indexOf("@"+myNickName)!=-1&&allAt[i].href.indexOf(myAccount)!=-1)
+		        {
+		            if(comment[j].getElementsByClassName("comments-list mod-comments-sub").length==0)
+		            {
+		            	content[0].getElementsByClassName("act-reply none")[0].click();
+		            	return 1;
+		            }
+		        }
+		    }
+		}
+		return 0;
+	}
+	//函数：进行回复
+	function reply()
 	{
-	    var content=comment[j].getElementsByClassName("comments-content");
-	    var allAt=content[0].getElementsByClassName("nickname name c_tx  q_namecard");
-	    for(var i=0;i<allAt.length;i++)
-	    {
-	        if((allAt[i].innerText).indexOf("@"+myNickName)!=-1)
-	        {
-	            if(comment[j].getElementsByClassName("comments-list mod-comments-sub").length==0)
-	            {
-	                content[0].getElementsByClassName("act-reply none")[0].click();
-	                var box=document.getElementsByClassName("comment-box-wrap")[0];
-	                box=document.getElementsByClassName("comment-box-wrap")[0];
-                	if(box==null)
-	                	console.log("click is null");
-	       			else
-	       			{
-                		box.getElementsByClassName("textinput textarea c_tx2")[0].innerText=autoReply;
-                		comment[j].getElementsByClassName("btn-post gb_bt  evt_click")[0].click();
-                		console.log("Just clicked auto-reply");
-	       			}
-	            }
-	        }
-	    }
+		var autoReply="[自动回复]本人空间已交由爬虫进行自动点赞和回复，有问题请直接联系我，谢谢。";
+	    var box=document.getElementsByClassName("comment-box-wrap")[0];
+	    box=document.getElementsByClassName("comment-box-wrap")[0];
+		if(box!=null)
+		{
+			box.getElementsByClassName("textinput textarea c_tx2")[0].innerText=autoReply;
+			document.getElementsByClassName("btn-post gb_bt  evt_click")[1].click();
+			console.log("Just clicked auto-reply");
+		}	
+	}
+	var atMe=0; 
+	atMe=search();
+	if(atMe==1)
+	{
+		reply();
 	}
 	"""
 
+def quit(driver):
+	driver.quit()
+	exit(0)
+
 def login(driver):
+	print ("Logging......")
+	driver.get("http://i.qq.com/")
+	time.sleep(3)
+	driver.switch_to_frame("login_frame")
+	time.sleep(3)
+	driver.find_element_by_id("switcher_plogin").click()
+	time.sleep(3)
+	driver.find_element_by_id("u").clear()  
+	driver.find_element_by_id("u").send_keys(ACCOUNT)
+	driver.find_element_by_id("p").clear()
+	driver.find_element_by_id("p").send_keys(PASSWORD)
+	time.sleep(3)
+	driver.find_element_by_id("login_button").click()
+	time.sleep(3)
 	try:
-		print ("Logging......")
-		driver.get("http://qzone.qq.com/")
-		time.sleep(3)
-		driver.switch_to_frame("login_frame")
-		time.sleep(3)
-		driver.find_element_by_id("switcher_plogin").click()
-		time.sleep(3)
-		driver.find_element_by_id("u").clear()  
-		driver.find_element_by_id("u").send_keys(ACCOUNT)
-		driver.find_element_by_id("p").clear()
-		driver.find_element_by_id("p").send_keys(PASSWORD)
-		time.sleep(3)
-		driver.find_element_by_id("login_button").click()
-		time.sleep(3)
-	except Exception as e:
-		print("Login failed,\nplease check your account & password & network.\n")
-		driver.close()
-		exit(0)
+		driver.find_element_by_id('feed_tab_hover')
+	except NoSuchElementException:
+		print("Login failed,\nplease check your account & password & network,\nor maybe you are limited from loginning.")
+		quit(driver)
 	else:
 		print("Login successfully.")
 		return driver
-
+	
 if __name__=="__main__":
-	driver = webdriver.PhantomJS()#window下需填入executable_path="浏览器绝对路径"属性	
+	driver = webdriver.PhantomJS()
 	while 1:
-		a=driver.execute_script("return document.getElementById('feed_tab_hover')")
-		if a is None:
+		try:
+			driver.find_element_by_id('feed_tab_hover')
+		except NoSuchElementException:
 			driver=login(driver)
-		driver.execute_script(ZAN)
-		driver.execute_script(REPLY)
-		log=driver.get_log("browser")
-		for i in log:
-			if (i["message"].find("clicked")!=-1):
-				print(str(i["message"]).encode("utf8"))
-		driver.refresh()
-		time.sleep(30)
+		else:
+			driver.execute_script(ZAN)
+			driver.execute_script(REPLY)
+			log=driver.get_log("browser")
+			for i in log:
+				if (i["message"].find("clicked")!=-1):
+					print(str(i["message"]).encode("utf8"))
+			driver.refresh()
+			time.sleep(30)
